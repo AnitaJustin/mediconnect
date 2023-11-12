c5db18c9f221378b6e3d49daaeeae656c0983ad1
Anita Justin <anitajustinc@gmail.com>
2023-11-12 22:09:27 +0530

    signin sucess

--- dem11app/views.py
+++ dem11app/views.py
@@ -2,23 +2,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import SignUpForm, SignInForm
-from django.contrib.messages import add_message,WARNING,success
+from django.contrib.messages import add_message,WARNING,success,info
 
 def homepage(request):
     return render(request,"homepage.html")
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data['password'])
-#             user.save()
-#             success(request, 'Account created successfully. You can now sign in.')
-#             return redirect('signin')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'signup.html', {'form': form})
-
 
 def signup(request):
     if request.method == 'POST':
@@ -40,45 +27,22 @@ def signup(request):
         else:
             return render(request, 'signup.html', {'form': form})
 
-# def signin(request):
-#     if request.method == 'POST':
-#         form = SignInForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 if login(request, user):
-#                     return redirect('dashboard')  # Redirect to your dashboard or any other page
-                
-
-    # else:
-    #     form = SignInForm()
-    # return render(request, 'signin.html', {'form': form})
-
 
 def signin(request):
     if request.method == 'POST':
         form = SignInForm(request.POST)
+
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
-            print(f"Username: {username}, Password: {password}, User: {user}")
 
             if user is not None:
-                print("User is not None, attempting login...")
-                login_success = login(request, user)
-                print(f"Login success: {login_success}")
-
-                if login_success:
-                    print("Login successful")
-                    return redirect('dashboard') 
-                else:
-                    print("Login function returned False after authentication")
-
+                login(request,user)
+                return redirect('dashboard') 
+            
             else:
-                print("Authentication failed")
+                info(request,'Invalid Credentials')
 
     else:
         form = SignInForm()
